8f247bea18f9dedf61c437d05520ef41ab3d60d2
MrPwet <beligat.fx@gmail.com>
2013-12-28 19:22:19 +0100

    Fixed LandSensorsTest -- Tout arcs

---
+++ TestRobot/robot/RobotTest.java
@@ -0,0 +1,14 @@
+package robot;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: MrPwet
+ * Date: 28/12/13
+ * Time: 17:52
+ */
+public class RobotTest {
+
+    public void TestGetXPosition() throws UnlandedRobotException {
+
+    }
+}
